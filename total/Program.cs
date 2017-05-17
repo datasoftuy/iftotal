aa5874538ae6943f3e0c1a75fec9f2e265eb5156
datasoftuy <andres.esteves.aharonian@gmail.com>
2017-05-17 12:02:46 -0300

    terminado

---
+++ total/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace total
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int cant = 0, total = 0;
+            //Ej1: Pedir la cantidad de números que desea sumar. Luego perdir cada uno de los
+            //números. Mostrar el resultado.
+            Console.Write("\t  * Ejercicio 1 *\n");
+            Console.Write("Ingrese la cantidad de valores a sumar: ");
+            cant = Convert.ToInt32(Console.ReadLine());
+            total = 0;
+            for (int i = 1; i <= cant; i++)
+            {
+                Console.Write("Ingrese un valor: ");
+                total = total + Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("La suma de los valores es:  " + total);
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
